85e404da5d655fabf4d0ebae1534e73264cc5add
Federico Caballero <98574058+fede-caballero@users.noreply.github.com>
2022-08-18 20:13:31 -0300

    Create Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,11 @@
+nombre = "Federico"
+apellido = "Caballero"
+edad = 31
+email = "federicorcaballero@gmail.com"
+tel = 542624522550
+casado = True
+con_hijos = True
+amigos = ["andres", "martin", "bruno"]
+peliculas_vistas = {"Pelicula1": "El padrino", "Pelicula2":"Toy Story"}
+
+print(nombre, apellido, edad, email, tel, casado, con_hijos, amigos, peliculas_vistas)
